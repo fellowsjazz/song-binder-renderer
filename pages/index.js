c98c42cad5d589e32334c3bc32019d66bdf15af7
fellowsjazz <russellmatthews32@gmail.com>
2023-06-07 17:53:05 -0400

    package update2

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Box, Text, Flex, Spacer, useDisclosure } from "@chakra-ui/react";
-import DisplayTBAofToken from "./components/displayTBAofToken";
+import DisplayTBAofToken from "./components/DisplayTBAofToken";
 
 const inter = Inter({ subsets: ["latin"] });
 

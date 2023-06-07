60be9be5a49a70f36266a6887f66c8dab127b025
fellowsjazz <russellmatthews32@gmail.com>
2023-06-07 17:57:26 -0400

    package update3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    return config
+  },
 }
-
-module.exports = nextConfig

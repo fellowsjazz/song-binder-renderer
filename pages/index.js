6398d3e1360e5a7067e4646c363f51976e8a6aa0
fellowsjazz <russellmatthews32@gmail.com>
2023-06-07 17:11:08 -0400

    drawer

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,66 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Flex, Spacer, useDisclosure } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <Box display={"flex"} flexDir={"row"} justifyContent={"center"}>
-      <Box h={"100vh"} w={"100vh"} border={"1px"} display={"flex"} flexDir={"column"} alignItems={"center"}>
-        <Text fontSize={"3xl"}>Camp 4 Song Binder</Text>
+    <Box display="flex" justifyContent="center">
+      <Box
+        h="100vh"
+        w="100vh"
+        border="1px"
+        display="flex"
+        flexDir="column"
+        alignItems="center"
+        position="relative"
+      >
+        <Text fontSize="3xl">Camp 4 Song Binder</Text>
+        <BottomTab />
       </Box>
     </Box>
+  );
+}
+
+function BottomTab() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Flex
+        position="absolute"
+        bottom={0}
+        right={0}
+        p={2}
+        bg="gray.200"
+        borderRadius="md"
+        cursor="pointer"
+        onMouseEnter={onOpen}
+        onMouseLeave={onClose}
+      >
+        <Text fontSize="sm">Click me</Text>
+        <Spacer />
+        <Text fontSize="sm">&#x25BE;</Text>
+      </Flex>
+      {isOpen && (
+        <Box
+          position="absolute"
+          bottom={0}
+          right={0}
+          p={4}
+          bg="white"
+          border="1px"
+          borderRadius="md"
+          boxShadow="md"
+          zIndex={1}
+          onMouseEnter={onOpen}
+          onMouseLeave={onClose}
+        >
+          <Text>Bottom Drawer Content</Text>
+        </Box>
+      )}
+    </>
   );
 }
